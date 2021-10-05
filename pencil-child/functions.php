aa3eaf078a7c0e57ba196f5a6e096aec89e08c0a
Snigdha29 <snigdha.twilight@gmail.com>
2021-10-05 14:53:38 +0530

    Add WooCommerce Cart

--- pencil-child/functions.php
+++ pencil-child/functions.php
@@ -1,4 +1,6 @@
 <?php
+
+//Add all css stylesheets
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
     $parenthandle = 'parent-style';
@@ -8,14 +10,14 @@ function my_theme_enqueue_styles() {
 
     wp_enqueue_style( 'child-style', get_stylesheet_uri().'style.css',
         array( $parenthandle ), $theme->get('Version'));
-
-   // wp_enqueue_style( 'woo-style', get_template_directory_uri().'/inc/woo_style.css', array( ), $theme->get('Version'));
 }
 
+//Add WooCommerce Support
 function my_theme_woocommerce_setup(){
     add_theme_support('woocommerce');
 }
 add_action( 'after_setup_theme', 'my_theme_woocommerce_setup' );
 
+//Include WooCommerce Functions
 include_once 'inc/woo_functions.php';
 ?>
--- pencil-child/inc/woo_functions.php
+++ pencil-child/inc/woo_functions.php
@@ -1,23 +1,82 @@
 <?php 
+    //Remove Sidebar
     remove_action('woocommerce_sidebar','woocommerce_get_sidebar');
     
+    //Remove Page title on Top
     add_filter('woocommerce_show_page_title', 'toggle_title');    
     function toggle_title($val) {
         $val = false;
         return $val;
     }
     
-    add_action( 'woocommerce_after_shop_loop_item_title', 'the_excerpt' ); 
+    //Show Product Excerpt 
+    add_action( 'woocommerce_after_shop_loop_item_title', 'the_excerpt' );
 
+    //Remove product count
     remove_action('woocommerce_before_shop_loop', 'woocommerce_result_count', 20);
 
-    add_filter('loop_shop_per_page','mytheme_products_per_page',20);
-    function mytheme_products_per_page($num_products){
+    // Show 20 products on a page
+    add_filter('loop_shop_per_page','pencil_products_per_page',20);
+    function pencil_products_per_page($num_products){
         return 20;        
     }
 
-    add_filter('loop_shop_columns','mytheme_product_columns',20);
-    function mytheme_product_columns($cols) {
+    //Show 5 products in a row
+    add_filter('loop_shop_columns','pencil_product_columns',20);
+    function pencil_product_columns($cols) {
         return 5;
     }
-?>
+
+    //Add Empty Cart Button
+    add_action('woocommerce_cart_actions','pencil_add_empty_cart_button');
+    function pencil_add_empty_cart_button() {
+        echo '<a class="button" href="?empty-cart=true">'._('Empty Cart','woocommerce').'</a>';
+    }
+
+    //Empty the cart when Empty Cart Button is clicked
+    add_action('init','pencil_empty_cart');
+    function pencil_empty_cart() {
+        global $woocommerce;
+        if (isset($_GET['empty-cart'])) {
+            $woocommerce->cart->empty_cart();
+        }
+    }
+
+    //Remove SKU from single product page
+    remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40);
+
+    //Add Cart to Menu with added products
+    function my_wc_cart_count() {
+ 
+        if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+     
+            $count = WC()->cart->cart_contents_count;
+            ?><a class="cart-contents" href="<?php echo WC()->cart->get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><?php
+            if ( $count > 0 ) {
+                ?>
+                <span class="cart-contents-count"><?php echo esc_html( $count ); ?></span>
+                <?php
+            }
+                    ?></a><?php
+        }
+     
+    }
+    add_action( 'woocommerce_before_main_content', 'my_wc_cart_count', 20);
+
+    function my_header_add_to_cart_fragment( $fragments ) {
+ 
+        ob_start();
+        $count = WC()->cart->cart_contents_count;
+        ?><a class="cart-contents" href="<?php echo WC()->cart->get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><?php
+        if ( $count > 0 ) {
+            ?>
+            <span class="cart-contents-count"><?php echo esc_html( $count ); ?></span>
+            <?php            
+        }
+            ?></a><?php
+     
+        $fragments['a.cart-contents'] = ob_get_clean();
+         
+        return $fragments;
+    }
+    add_filter( 'woocommerce_add_to_cart_fragments', 'my_header_add_to_cart_fragment' );
